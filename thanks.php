c73d20222145997c3e5c368708439ff367ea0241
Mtuhiko Gusikenn <chanmitsu35@gmail.com>
2020-06-03 16:14:50 +0900

    完成しそう

---
+++ thanks.php
@@ -0,0 +1,30 @@
+<?php
+
+// var_dump($_POST);
+
+// 別のファイルの読み込み
+// require_once(読み込みたいファイルのパス);
+require_once('functions.php');
+
+
+// 入力された内容を取得
+$username = $_POST['username'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>お問い合わせありがとうございました</h1>
+    <p>お名前:<?= h($username); ?></p>
+    <p>メールアドレス:<?= h($email); ?></p>
+    <p>お問い合わせ内容:<?= h($content) ?></p>
+</body>
+</html>
